4c2b1b8cf95d903f9434aa8593f8e808285bed4f
nestast <94304109+nestast@users.noreply.github.com>
2026-05-25 22:06:41 +0200

    Correctif : Ajout de sécurité au hachage des mots de passe avec sel et itérations.

--- lib/utils/password_utils.dart
+++ lib/utils/password_utils.dart
@@ -1,8 +1,13 @@
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
 
+const String SALT = 'your-secure-salt-change-me';
+
 String hashPassword(String password) {
-  return sha256.convert(utf8.encode(password)).toString();
+  // Ajout d'un sel et plusieurs itérations pour plus de sécurité
+  final bytes = utf8.encode(password + SALT);
+  final digest = sha256.convert(bytes);
+  return sha256.convert(utf8.encode(digest.toString() + SALT)).toString();
 }
 
 bool verifyPassword(String password, String storedHash) {
